51423c4b0b35d46a013b167110571e18e52d5da6
Md Samir Hossain <samirhossain9093@gmail.com>
2026-05-26 21:36:23 +0530

    Changes

--- Backend/server.js
+++ Backend/server.js
@@ -13,8 +13,9 @@ const PORT = 3000;
 
 // MIDDLEWARES
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: "*",
+    // origin: "http://localhost:5173",
+    // credentials: true
 }));
 app.use(clerkMiddleware());
 app.use(express.json({limit: '20mb'}));
